897ec45056dbcc1cd7d2c2d73af64f02d5675171
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-09 17:41:04 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using CSSimpleFunctions;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
+
+namespace AcciTrack_Startup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists("resources"))
+            {
+                SimpleFileHandler.ProjectToLocation(Assembly.GetExecutingAssembly(), "AcciTrack_Build.zip");
+                ZipFile.ExtractToDirectory("AcciTrack_Build.zip", "resources");
+                File.Delete("AcciTrack_Build.zip");
+            }
+            ProcessStartInfo run0 = new ProcessStartInfo();
+            run0.WorkingDirectory = "resources";
+            run0.FileName = "resources\\AcciTrack_Build.exe";
+            run0.UseShellExecute = false;
+            run0.RedirectStandardOutput = true;
+            run0.CreateNoWindow = true;
+            Process.Start(run0);
+        }
+    }
+}
